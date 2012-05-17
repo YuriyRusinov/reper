2939165a00e9b25efbe2d0924610725a2f47f60d
skarin <skarin@05742d63-fc59-4ccd-b3f0-d7e2a2a8a88d>
2012-05-17 04:15:52 +0000

    версия 0.13.4
    git-svn-id: svn+ssh://svn.code.sf.net/p/dynamicdocs/code@76 05742d63-fc59-4ccd-b3f0-d7e2a2a8a88d

--- ddocclient/kkscore/kksclient_name.h
+++ ddocclient/kkscore/kksclient_name.h
@@ -2,7 +2,7 @@
 #define KKSCLIENT_NAME_H
 #define EXECUTABLE_PREFIX "ddocclient" 
 #define ORGANIZATION "DynamicSoft" 
-#define PRODUCT_FULL_NAME "DynamicDocs Client 0.13.3" 
+#define PRODUCT_FULL_NAME "DynamicDocs Client 0.13.4" 
 #define PRODUCT_NAME "DynamicDocs Client" 
-#define KKS_VERSION "0.13.3"
+#define KKS_VERSION "0.13.4"
 #endif
