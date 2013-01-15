b4e4fff8cc983d46e796a8052a587359fe599275
yuriyrusinov <yuriyrusinov@05742d63-fc59-4ccd-b3f0-d7e2a2a8a88d>
2013-01-15 13:35:59 +0000

    New searchtemplates model development has begun.
    Signed-off-by: Yuriy Rusinov <yrusinov@gmail.com>
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/dynamicdocs/code@374 05742d63-fc59-4ccd-b3f0-d7e2a2a8a88d

---
+++ ddocclient/kksgui/KKSSearchTemplateModel.cpp
@@ -0,0 +1,15 @@
+/* 
+ * File:   KKSSearchTemplateModel.cpp
+ * Author: rusinov
+ * 
+ * Created on 14 январь 2013 г., 13:52
+ */
+
+#include "KKSSearchTemplateModel.h"
+
+KKSSearchTemplateModel::KKSSearchTemplateModel() {
+}
+
+KKSSearchTemplateModel::~KKSSearchTemplateModel() {
+}
+
---
+++ ddocclient/kksgui/KKSSearchTemplateModel.h
@@ -0,0 +1,44 @@
+/* 
+ * File:   KKSSearchTemplateModel.h
+ * Author: rusinov
+ *
+ * Created on 14 январь 2013 г., 13:52
+ */
+
+#ifndef KKSSEARCHTEMPLATEMODEL_H
+#define	KKSSEARCHTEMPLATEMODEL_H
+
+#include <QAbstractItemModel>
+#include <QModelIndex>
+#include <QVariant>
+
+#include "kksgui_config.h"
+
+class _GUI_EXPORT KKSSearchTemplateModel : public QAbstractItemModel
+{
+public:
+    KKSSearchTemplateModel(const KKSMap<int, KKSSearchTemplateType *>& sTypes, QObject * parent=0);
+    virtual ~KKSSearchTemplateModel();
+
+    QVariant data (const QModelIndex &index, int role) const;
+    QVariant headerData (int section, Qt::Orientation orientation,
+                         int role = Qt::DisplayRole) const;
+
+    QModelIndex index (int row, int column, const QModelIndex& parent=QModelIndex()) const;
+    QModelIndex parent (const QModelIndex &index) const;
+
+    int rowCount (const QModelIndex& parent=QModelIndex()) const;
+    int columnCount (const QModelIndex& parent=QModelIndex()) const;
+
+    Qt::ItemFlags flags (const QModelIndex& index) const;
+    bool setData (const QModelIndex& index, const QVariant& value, int role = Qt::EditRole);
+
+    bool insertRows (int row, int count, const QModelIndex& parent = QModelIndex() );
+    bool removeRows (int row, int count, const QModelIndex& parent = QModelIndex() );
+
+private:
+
+};
+
+#endif	/* KKSSEARCHTEMPLATEMODEL_H */
+
