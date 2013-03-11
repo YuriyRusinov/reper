6bbe253ecaebe5ddd0f193b1a08673a4142d7a30
yuriyrusinov <yuriyrusinov@05742d63-fc59-4ccd-b3f0-d7e2a2a8a88d>
2013-03-11 09:40:50 +0000

    Add search template form was developped.
    Signed-off-by: Yuriy Rusinov <yrusinov@gmail.com>
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/dynamicdocs/code@411 05742d63-fc59-4ccd-b3f0-d7e2a2a8a88d

--- ddocclient/kksgui/ui/savesearchtemplateform.cpp
+++ ddocclient/kksgui/ui/savesearchtemplateform.cpp
@@ -1,3 +1,8 @@
+#include <QAbstractItemModel>
+#include <QItemSelectionModel>
+#include <QModelIndex>
+#include <QtDebug>
+
 #include "savesearchtemplateform.h"
 #include "ui_save_search_template_form.h"
 
@@ -6,9 +11,45 @@ SaveSearchTemplateForm :: SaveSearchTemplateForm (QWidget * parent, Qt::WindowFl
     UI (new Ui::save_search_template_form)
 {
     UI->setupUi (this);
+
+    catChStateChanged (Qt::Unchecked);
+    connect (UI->chCategory, SIGNAL (stateChanged (int)), this, SLOT (catChStateChanged (int)) );
+
+    connect (UI->pbOk, SIGNAL (clicked()), this, SLOT (accept()) );
+    connect (UI->pbCancel, SIGNAL (clicked()), this, SLOT (reject()) );
 }
 
 SaveSearchTemplateForm :: ~SaveSearchTemplateForm (void)
 {
     delete UI;
+}
+
+void SaveSearchTemplateForm :: setCategoryModel (QAbstractItemModel * catMod)
+{
+    QAbstractItemModel * oldMod = UI->tvCategory->model ();
+    UI->tvCategory->setModel (catMod);
+    if (oldMod && oldMod != catMod)
+        delete oldMod;
+}
+
+int SaveSearchTemplateForm :: getIdCat (void) const
+{
+    if (!UI->tvCategory->isEnabled())
+        return -1;
+
+    QItemSelectionModel * selMod = UI->tvCategory->selectionModel ();
+    QModelIndex wIndex = selMod->currentIndex ();
+    wIndex = wIndex.sibling (wIndex.row(), 0);
+    return wIndex.isValid() ? wIndex.data (Qt::UserRole).toInt() : -1;
+}
+
+void SaveSearchTemplateForm :: catChStateChanged (int state)
+{
+    bool isEn (state == Qt::Checked);
+    UI->tvCategory->setEnabled (isEn);
+}
+
+QString SaveSearchTemplateForm :: getName (void) const
+{
+    return UI->lEName->text();
 }
--- ddocclient/kksgui/ui/savesearchtemplateform.h
+++ ddocclient/kksgui/ui/savesearchtemplateform.h
@@ -9,13 +9,25 @@ namespace Ui
     class save_search_template_form;
 };
 
+class QAbstractItemModel;
+
 class SaveSearchTemplateForm : public QDialog
 {
 public:
     SaveSearchTemplateForm (QWidget * parent=0, Qt::WindowFlags flags=0);
     virtual ~SaveSearchTemplateForm (void);
 
+    void setCategoryModel (QAbstractItemModel * catMod);
+    int getIdCat (void) const;
+    QString getName (void) const;
+
+private slots:
+    void catChStateChanged (int state);
+
 private:
+    //
+    // Variables
+    //
     Ui::save_search_template_form * UI;
 
 private:
