17d299bf113e073aef40d0783c1dd7dc0791407f
skarin <skarin@05742d63-fc59-4ccd-b3f0-d7e2a2a8a88d>
2013-07-15 07:00:24 +0000

    версия 1.2.2 + добавлена полная установка со всеми базовыми библиотеками и СУБД + пространственная база данных
    git-svn-id: svn+ssh://svn.code.sf.net/p/dynamicdocs/code@579 05742d63-fc59-4ccd-b3f0-d7e2a2a8a88d

--- ddocserver/install.sh
+++ ddocserver/install.sh
@@ -12,6 +12,53 @@ INTERNALS=./internals
 
 PASSWD=""
 
+checkSystem()
+{
+    EXIST_PORT=`ps afx | grep postmaster | head -1 | grep -o $LINTER_PORT`
+    if [ "$EXIST_PORT" = "$LINTER_PORT" ]; then
+        printf "\n\nERROR! There are any postmaster process running on port $LINTER_PORT!\n"
+        printf "Change LINTER_PORT parameter in user.config file\n"
+        printf "Installation aborted!\n\n"
+        exit 1
+    fi
+    
+    EXIST_PORT=`ps afx | grep postmaster | head -1 | grep -o $LINTER_PORT`
+    if [ "$EXIST_PORT" = "$LINTER_PORT" ]; then
+        printf "\n\nERROR! There are any postmaster process running on port $LINTER_PORT!\n"
+        printf "Change LINTER_PORT parameter in user.config file\n"
+        printf "Installation aborted!\n\n"
+        exit 1
+    fi
+
+    if test -e $LINTER_PREFIX; then
+	#file exist
+	
+	if ! test -d $LINTER_PREFIX; then
+	    printf "\n\nERROR! /full/path/to/$PROJECT_NAME Server MUST be a directory!\n"
+            printf "Change LINTER_ROOT parameter in user.config file\n"
+            printf "Installation aborted!\n\n"
+            exit 1
+	fi
+    
+        #exist and is a dir
+	
+        NFILES=`ls -a $LINTER_PREFIX | wc -l`
+	if [ $NFILES != 2 ]; then
+	    printf "/full/path/to/$PROJECT_NAME Server MUST be empty!\n"
+            printf "Change LINTER_ROOT parameter in user.config file\n"
+            printf "Installation aborted!\n\n"
+            exit 1
+	fi    
+	
+	PREV=`pwd -P`
+	cd $LINTER_PREFIX
+	LINTER_PREFIX=`pwd -P`
+	cd $PREV
+	
+    fi
+    
+}
+
 createPGPass()
 {
     read -s -p "Input password for user $USER: " PASSWD
@@ -36,6 +83,22 @@ show_complete()
     printf "********************************************************************************\n"
 }
 
+show_complete_full()
+{
+    printf "********************************************************************************\n"
+    printf "********************************************************************************\n"
+    printf "********************************************************************************\n"
+    printf "\nPostgreSQL Server and all libraries successfully installed.\n"
+    printf "Now PostgreSQL Server is runnig on port $LINTER_PORT\n"
+    printf "For automatical running PostgreSQL Server when operation system is starting up\n"
+    printf "you can use ./system_files/postgresql script\n\n"
+    
+    printf "               $PROJECT_NAME Server full installation complete.\n"
+    printf "********************************************************************************\n"
+    printf "********************************************************************************\n"
+    printf "********************************************************************************\n"
+}
+
 show_error()
 {
     printf "********************************************************************************\n"
@@ -51,7 +114,7 @@ show_usage()
 {
     printf "\n\n"
     printf "$PROJECT_NAME Server $VERSION installer\n\n"
-    printf "Usage: $1 [install|update|clean|help]"
+    printf "Usage: $1 [install|fullinstall|update|clean|help]"
     printf "\n\n"
     
     printf "Using without parameters is synonym for ./install.sh install\n\n\n"
@@ -59,6 +122,18 @@ show_usage()
     printf "\t\t\tInstall $PROJECT_NAME Server and create new database\n"    
     printf "\t\t\tNote that if the File Library was already compiled\n"
     printf "\t\t\tthey will be only linked and installed (make install)\n"
+    printf "\n\n"
+    
+    printf "fullinstall \t- \tBuild and install PostgreSQL database server from sources.\n"
+    printf "\t\t\tAlso next libraries will be installed from sources:\n"
+    printf "\t\t\t\t- PROJ4 - cartographic projection software;\n"
+    printf "\t\t\t\t- GEOS - Geometry Engine Open Source;\n"
+    printf "\t\t\t\t- GDAL - GeoData Abstraction Layer;\n"
+    printf "\t\t\t\t- GSL - GNU Scientific Library;\n"
+    printf "\t\t\t\t- OSSP uuid - Universally Unique Identifier;\n"
+    printf "\t\t\t\t- PostGIS - Geographic Information Systems Extensions to PostgreSQL.\n"
+    printf "\t\t\tThen build $PROJECT_NAME File Library, install $PROJECT_NAME Server\n"
+    printf "\t\t\tand create new database\n"
     printf "\n\n"
     
     printf "update \t\t- \tUpdate $PROJECT_NAME Server from previous to last version\n"
@@ -106,9 +181,35 @@ case $COMMAND in
 	cd $PREV
 	exit 0
     ;;
+    fullinstall)
+        checkSystem
+	createPGPass
+        cd $SYS_FILES &&
+        SYS_FILES_ABS=`pwd -P` &&
+        cd $SYS_FILES_ABS &&
+	chmod a+x ./build-src.sh &&
+	./build-src.sh "$VERSION" "$LINTER_PREFIX" "$PGDATA_DIR" "$LINTER_PORT" "$SYS_FILES_ABS/src" "template1" "$USER" "$PASSWD" &&
+	chmod a+x ./build.sh &&
+	./build.sh "$VERSION" "$LINTER_PREFIX" "$PGDATA_DIR" "$FILE_ARCH" "0" "$AUTO_CREATE" "$PROJECT_NAME" &&
+	chmod a+x ./createdb.sh &&
+	./createdb.sh "$BASE" "$USER" "$LINTER_PORT" "$LINTER_PREFIX" "$ENCODING" "1" "$PASSWD" "$VERSION" "$LOCAL_ADDRESS" "$IO_FILES" "$USE_MODULES" "$IS_MAIN_ORG" "$ORG_NAME" "$LOCAL_PORT" 2>$PREV/createdb.log &&
+	chmod a+x ./createanalyzer.sh &&
+	./createanalyzer.sh "$BASE" "$USER" "$LINTER_PORT" "$LINTER_PREFIX" "$PGDATA_DIR" &&
+	chmod a+x ./createdumper.sh &&
+	./createdumper.sh "$BASE" "$USER" "$LINTER_PORT" "$LINTER_PREFIX" "$PGDATA_DIR" &&
+	INSTALL_OK=1
+	deletePGPass
+
+        if [ $INSTALL_OK = 1 ]; then
+            show_complete_full
+            exit 0
+        fi
+    ;;
     install)
 	createPGPass
         cd $SYS_FILES &&
+        SYS_FILES_ABS=`pwd -P` &&
+        cd $SYS_FILES_ABS &&
 	chmod a+x ./build.sh &&
 	./build.sh "$VERSION" "$LINTER_PREFIX" "$PGDATA_DIR" "$FILE_ARCH" "0" "$AUTO_CREATE" "$PROJECT_NAME" &&
 	chmod a+x ./createdb.sh &&
---
+++ ddocserver/system_files/build-src.sh
@@ -0,0 +1,198 @@
+#!/bin/bash
+
+VERSION=$1
+PREFIX=$2
+PGDATA=$3
+PORT=$4
+SRC_DIR=$5
+DB_NAME=$6
+USER=$7
+PASSWORD=$8
+
+
+#SRC_DIR=/home/ddoc/src
+
+
+PROJ_VER=4.8.0
+GEOS_VER=3.3.8
+PGSQL_VER=9.2.3
+POSTGIS_VER=2.0.3
+GDAL_VER=1.9.2
+UUID_VER=1.6.2
+GSL_VER=1.14
+
+PG_SRC_DIR=$SRC_DIR/postgresql-$PGSQL_VER
+UUID_SRC_DIR=$SRC_DIR/uuid-$UUID_VER
+GSL_SRC_DIR=$SRC_DIR/gsl-$GSL_VER
+PROJ4_SRC_DIR=$SRC_DIR/proj-$PROJ_VER
+GEOS_SRC_DIR=$SRC_DIR/geos-$GEOS_VER
+GDAL_SRC_DIR=$SRC_DIR/gdal-$GDAL_VER
+POSTGIS_SRC_DIR=$SRC_DIR/postgis-$POSTGIS_VER
+
+SYS_FILES_ABS=`pwd -P`
+
+cd $SRC_DIR
+PREV=`pwd -P`
+
+if [ ! -f ./proj-$PROJ_VER.tar.gz ]; then 
+    printf "Can't find ./proj-$PROJ_VER.tar.gz in $SRC_DIR\n";
+    exit 1
+fi
+
+if [ ! -f ./geos-$GEOS_VER.tar.bz2 ]; then 
+    printf "Can't find ./geos-$GEOS_VER.tar.gz in $SRC_DIR\n";
+    exit 1
+fi
+
+if [ ! -f ./postgresql-$PGSQL_VER.tar.bz2 ]; then 
+    printf "Can't find ./postgresql-$PGSQL_VER.tar.bz2 in $SRC_DIR\n";
+    exit 1
+fi
+
+if [ ! -f ./postgis-$POSTGIS_VER.tar.gz ]; then 
+    printf "Can't find ./postgis-$POSTGIS_VER.tar.gz in $SRC_DIR\n";
+    exit 1
+fi
+
+if [ ! -f ./gdal-$GDAL_VER.tar.gz ]; then 
+    printf "Can't find ./gdal-$GDAL_VER.tar.gz in $SRC_DIR\n";
+    exit 1
+fi
+
+if [ ! -f ./uuid-$UUID_VER.tar.gz ]; then 
+    printf "Can't find ./uuid-$UUID_VER.tar.gz in $SRC_DIR\n";
+    exit 1
+fi
+
+if [ ! -f ./gsl-$GSL_VER.tar.gz ]; then 
+    printf "Can't find ./gsl-$GSL_VER.tar.gz in $SRC_DIR\n";
+    exit 1
+fi
+
+
+#PREFIX=$LINTER_PREFIX
+
+PG_PREFIX=$PREFIX
+UUID_PREFIX=$PREFIX
+PROJ4_PREFIX=$PREFIX
+GEOS_PREFIX=$PREFIX
+GDAL_PREFIX=$PREFIX
+POSTGIS_PREFIX=$PREFIX
+
+GSL_PREFIX=/usr/
+
+adduser postgres
+
+
+
+export LD_LIBRARY_PATH=$UUID_PREFIX/lib:$PREFIX/lib:$GSL_PREFIX
+
+#gunzip < uuid-$UUID_VER.tar.gz | tar xf - &&
+cd $UUID_SRC_DIR && 
+./configure LD_OPTIONS="-L$UUID_PREFIX/lib" --prefix=$UUID_PREFIX --with-pgsql=no --with-cxx && 
+make && 
+make install && 
+
+cd $PREV &&
+#bunzip2 < postgresql-$PGSQL_VER.tar.bz2 | tar xf - &&
+cd $PG_SRC_DIR && 
+./configure --with-ossp-uuid --prefix=$PG_PREFIX --with-libxml --with-libxslt && 
+make world && 
+make install && 
+cd ./contrib && 
+make && 
+make install &&
+
+
+mkdir $PGDATA && 
+chown -R postgres:postgres  $PGDATA && 
+su - postgres -c "$PG_PREFIX/bin/initdb -D $PGDATA -E UTF8 --locale=ru_RU.UTF8" &&
+
+cd $PREV %%
+#gunzip < gsl-$GSL_VER.tar.gz | tar xf - &&
+cd $GSL_SRC_DIR &&
+./configure --prefix=$GSL_PREFIX &&
+make &&
+make install &&
+
+cd $PREV &&
+#gunzip < proj-$PROJ_VER.tar.gz | tar xf - &&
+cd $PROJ4_SRC_DIR &&
+./configure --prefix=$PROJ4_PREFIX &&
+make &&
+make install &&
+
+cd $PREV &&
+#bunzip2 < geos-$GEOS_VER.tar.bz2 | tar xf - &&
+cd $GEOS_SRC_DIR &&
+./configure --prefix=$GEOS_PREFIX &&
+make &&
+make install &&
+
+cd $PREV &&
+#gunzip < gdal-$GDAL_VER.tar.gz | tar xf - &&
+cd $GDAL_SRC_DIR &&
+./configure --prefix=$GDAL_PREFIX --with-geos=$GEOS_PREFIX/bin/geos-config &&
+make &&
+make install &&
+
+cd $PREV &&
+#gunzip < postgis-$POSTGIS_VER.tar.gz | tar xf - &&
+cd $POSTGIS_SRC_DIR &&
+./configure --prefix=$POSTGIS_PREFIX --with-projdir=$PROJ4_PREFIX --with-geosconfig=$GEOS_PREFIX/bin/geos-config --with-pgconfig=$PG_PREFIX/bin/pg_config --with-gdalconfig=$GDAL_PREFIX/bin/gdal-config &&
+make &&
+make install &&
+
+cd $SYS_FILES_ABS &&
+CONF_FILES="$SYS_FILES_ABS/pg_hba.conf $SYS_FILES_ABS/postgresql.conf"
+sudo cp -f $CONF_FILES $PGDATA
+
+printf "#!/bin/sh\n" > ./postgresql &&
+printf "# postgresql    This is the init script for starting up the PostgreSQL\n" >> ./postgresql &&
+printf "#               server\n" >> ./postgresql &&
+printf "#\n" >> ./postgresql &&
+printf "# chkconfig: - 64 36\n" >> ./postgresql &&
+printf "# description: Starts and stops the PostgreSQL backend daemon that handles \\\n" >> ./postgresql &&
+printf "#              all database requests.\n" >> ./postgresql &&
+printf "# processname: postmaster\n" >> ./postgresql &&
+printf "# pidfile: /var/run/postmaster.pid\n" >> ./postgresql &&
+
+printf "\nPGVERSION=$PGSQL_VER\nPG_PREFIX=$PG_PREFIX\nPGPORT=$PORT\n\n" >> ./postgresql &&
+cat ./postgresql.orig >> ./postgresql &&
+chmod a+x ./postgresql &&
+cp ./postgresql /etc/init.d/ 
+
+OS_NAME=`uname -a | grep -o gentoo`
+SERVICE_ADDED=0
+if [ "$OS_NAME" = "gentoo" ]; then
+    rc_update add postgresql default &&
+    SERVICE_ADDED=1
+else
+    chkconfig --add postgresql &&
+    SERVICE_ADDED=1
+fi
+
+SERVICE_STARTED=0
+if [ $SERVICE_ADDED = 1 ]; then
+    service postgresql start &&
+    SERVICE_STARTED=1
+else
+    ./pg_start.sh start $PGSQL_VER $PG_PREFIX $PORT &&
+    SERVICE_STARTED=1
+fi
+
+if [ $SERVICE_STARTED = 0 ]; then
+    printf "\n\nERROR! Could not start PostgreSQL Server!\n\n"
+    exit 1
+fi
+
+sleep 5 &&
+
+$PG_PREFIX/bin/psql -U postgres -p $PORT -c "create role $USER with superuser createdb createrole login password '$PASSWORD'" -d template1
+
+$PG_PREFIX/bin/psql -U $USER -p $PORT -f $POSTGIS_SRC_DIR/postgis/postgis.sql -d $DB_NAME &&
+$PG_PREFIX/bin/psql -U $USER -p $PORT -f $POSTGIS_SRC_DIR/raster/rt_pg/rtpostgis.sql -d $DB_NAME &&
+
+#./pg_start.sh stop $PGSQL_VER $PG_PREFIX $PORT &&
+
+cd $SYS_FILES_ABS
