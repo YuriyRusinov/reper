2f5082621c9c95aacef2d7cf2992e37410985c08
yuriyrusinov <yuriyrusinov@05742d63-fc59-4ccd-b3f0-d7e2a2a8a88d>
2013-09-06 15:15:59 +0000

    Updates were added.
    Signed-off-by: Yuriy Rusinov <yrusinov@gmail.com>
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/dynamicdocs/code@639 05742d63-fc59-4ccd-b3f0-d7e2a2a8a88d

--- ddocserver/internals/updates/1.2.3_to_1.2.3.sql
+++ ddocserver/internals/updates/1.2.3_to_1.2.3.sql
@@ -10,4 +10,10 @@ select setAsNotLogging(2);
 
 select createTriggerUID ('criteria');
 
+\i ./functions/categories/agetrefvalue.sql
+\i ./functions/general/getrecattrvalue.sql
+drop function getioattrvaluehistory(integer, timestamp without time zone, timestamp without time zone);
+\i ./functions/io_objects/getioattrvaluehistory.sql
+\i ./functions/io_objects/getioattrvalue.sql
+
 commit;
