47c326c7df9611d76ee32e1b90d16aa798c1f5d0
yuriyrusinov <yuriyrusinov@05742d63-fc59-4ccd-b3f0-d7e2a2a8a88d>
2013-11-11 12:15:20 +0000

    Trigger was corrected.
    Signed-off-by: Yuriy Rusinov <yrusinov@gmail.com>
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/dynamicdocs/code@715 05742d63-fc59-4ccd-b3f0-d7e2a2a8a88d

--- ddocserver/internals/functions/categories/triggers/trgacrecinsert.sql
+++ ddocserver/internals/functions/categories/triggers/trgacrecinsert.sql
@@ -5,14 +5,45 @@ declare
 
     idMainCategory bigint;
     idType bigint;
+    isTableCat boolean;
+    cnt bigint;
 begin
 
     idCategory := new.id_io_category;
     select into idType id_io_category_type from io_categories where id=idCategory;
     if (idType = 10) then
+        select into idMainCategory id from io_categories where id_child = idCategory;
+        if (idMainCategory is not null) then
+            ---
+            --- table category
+            ---
+            isTableCat := true;
+        else
+            select into idMainCategory id from io_categories where id_child2 = idCategory;
+            if (idMainCategory is null) then
+                raise exception 'Incorrect category';
+                return null;
+            end if;
+            isTableCat := false;
+        end if;
+    else
+        idMainCategory := idCategory;
+        isTableCat := false;
+    end if;
+    if (isTableCat) then
+        select into cnt count(*) from io_objects where id_io_category = idMainCategory;
+        if (cnt > 0) then
+            raise exception 'Insert attribute into category with existing objects.';
+            return null;
+        end if;
+    else
     end if;
 
     return new;
 end
 $BODY$
 language 'plpgsql';
+
+select f_safe_drop_trigger('trgacinsert', 'attrs_categories');
+
+select f_create_trigger('trgacinsert', 'before', 'insert or update', 'attrs_categories', 'arecinsertcheck()');
