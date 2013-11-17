3da26fc319b431d4f233ca6f2573c940eba656be
yuriyrusinov <yuriyrusinov@05742d63-fc59-4ccd-b3f0-d7e2a2a8a88d>
2013-11-17 21:41:30 +0000

    Trigger for insert/update new attribute into existing category.
    Signed-off-by: Yuriy Rusinov <yrusinov@gmail.com>
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/dynamicdocs/code@737 05742d63-fc59-4ccd-b3f0-d7e2a2a8a88d

--- ddocserver/internals/functions/categories/triggers/trgacrecinsert.sql
+++ ddocserver/internals/functions/categories/triggers/trgacrecinsert.sql
@@ -31,12 +31,39 @@ begin
         isTableCat := false;
     end if;
     if (isTableCat) then
+        --
+        -- insert attribute into table category
+        --
         select into cnt count(*) from io_objects where id_io_category = idMainCategory;
-        if (cnt > 0) then
-            raise exception 'Insert attribute into category with existing objects.';
+        if (cnt = 0) then
+            --
+            -- IOs with tables for this category do not exist
+            --
+            return new;
+        elsif (cnt > 0 and new.is_mandatory and new.def_value is null ) then
+            raise exception 'Insert mandatory attribute with null default values into category % with existing objects.', idMainCategory;
+            return null;
+        elsif (cnt > 0 and (not new.is_mandatory) or (new.is_mandatory and new.def_value is not null)) then
+            raise warning 'Insert nonmandatory attribute or mandatory attribute with default value ';
+            --
+            -- TODO: mechanism of existing tables update has to be developped.
+            --
             return null;
         end if;
     else
+        --
+        -- insert attribute into rec_attrs_category
+        --
+        select count(*) into cnt from io_objects io inner join io_objects_organization ioo on (io.id=ioo.id_io_objects and io.id_owner_org!=ioo.id_organization and io.id_io_category= idMainCategory);
+        if (cnt = 0 or TG_OP = 'UPDATE') then
+            --
+            -- syncronization did not make
+            --
+            return new;
+        elsif (cnt > 0) then
+            raise exception 'Insert indicator into category with syncronized objects.';
+            return null;
+        end if;
     end if;
 
     return new;
