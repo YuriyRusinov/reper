5600537a5db1d6051d191d7260fe59ad668affb8
yuriyrusinov <yuriyrusinov@05742d63-fc59-4ccd-b3f0-d7e2a2a8a88d>
2013-12-05 13:53:45 +0000

    Some experiments.
    Signed-off-by: Yuriy Rusinov <yrusinov@gmail.com>
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/dynamicdocs/code@762 05742d63-fc59-4ccd-b3f0-d7e2a2a8a88d

--- ddocserver/internals/libfloader/src/hist.c
+++ ddocserver/internals/libfloader/src/hist.c
@@ -9,7 +9,35 @@ Datum histogram(PG_FUNCTION_ARGS)
 {
     unsigned long int num = PG_GETARG_UINT32(0);
 
+    float8 * result = (float8 *) (palloc (sizeof(float8)));
+
+    *result= 5.0;
+
     //gsl_histogram * gHist = gsl_histogram_alloc (num);
 
-    PG_RETURN_POINTER (0);//gHist);
+    PG_RETURN_POINTER (result);//gHist);
+}
+
+typedef struct Complex {
+    double      x;
+    double      y;
+} Complex;
+
+Datum complex_in(PG_FUNCTION_ARGS)
+{
+    char       *str = PG_GETARG_CSTRING(0);
+    double      x,
+                y;
+    Complex    *result;
+
+    if (sscanf(str, " ( %lf , %lf )", &x, &y) != 2)
+        ereport(ERROR,
+                (errcode(ERRCODE_INVALID_TEXT_REPRESENTATION),
+                 errmsg("invalid input syntax for complex: \"%s\"",
+                        str)));
+
+    result = (Complex *) palloc(sizeof(Complex));
+    result->x = x;
+    result->y = y;
+    PG_RETURN_POINTER(result);
 }
