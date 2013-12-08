8734d16597d86ba6f0a92fc706f9e7c953945b19
yuriyrusinov <yuriyrusinov@05742d63-fc59-4ccd-b3f0-d7e2a2a8a88d>
2013-12-08 20:53:19 +0000

    1st test.
    Signed-off-by: Yuriy Rusinov <yrusinov@gmail.com>
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/dynamicdocs/code@765 05742d63-fc59-4ccd-b3f0-d7e2a2a8a88d

--- ddocserver/internals/libfloader/src/hist.c
+++ ddocserver/internals/libfloader/src/hist.c
@@ -9,76 +9,12 @@ Datum histogram(PG_FUNCTION_ARGS)
 {
     unsigned long int num = PG_GETARG_UINT32(0);
 
-    float8 * result = (float8 *) (palloc (sizeof(float8)));
+    //float8 * result = (float8 *) (palloc (sizeof(float8)));
 
-    *result= 5.0;
+    //*result= 5.0;
 
-    //gsl_histogram * gHist = gsl_histogram_alloc (num);
+    gsl_histogram * gHist = gsl_histogram_alloc (num);
+    Datum d = PointerGetDatum (gHist);
 
-    PG_RETURN_POINTER (result);//gHist);
-}
-
-typedef struct Complex {
-    double      x;
-    double      y;
-} Complex;
-
-Datum complex_in(PG_FUNCTION_ARGS)
-{
-    char       *str = PG_GETARG_CSTRING(0);
-    double      x,
-                y;
-    Complex    *result;
-
-    if (sscanf(str, " ( %lf , %lf )", &x, &y) != 2)
-        ereport(ERROR,
-                (errcode(ERRCODE_INVALID_TEXT_REPRESENTATION),
-                 errmsg("invalid input syntax for complex: \"%s\"",
-                        str)));
-
-    result = (Complex *) palloc(sizeof(Complex));
-    result->x = x;
-    result->y = y;
-    PG_RETURN_POINTER(result);
-}
-
-PG_FUNCTION_INFO_V1(complex_out);
-
-Datum
-complex_out(PG_FUNCTION_ARGS)
-{
-    Complex    *complex = (Complex *) PG_GETARG_POINTER(0);
-    char       *result;
-
-    result = (char *) palloc(100);
-    snprintf(result, 100, "(%g,%g)", complex->x, complex->y);
-    PG_RETURN_CSTRING(result);
-}
-
-PG_FUNCTION_INFO_V1(complex_recv);
-
-Datum
-complex_recv(PG_FUNCTION_ARGS)
-{
-    StringInfo  buf = (StringInfo) PG_GETARG_POINTER(0);
-    Complex    *result;
-
-    result = (Complex *) palloc(sizeof(Complex));
-    result->x = pq_getmsgfloat8(buf);
-    result->y = pq_getmsgfloat8(buf);
-    PG_RETURN_POINTER(result);
-}
-
-PG_FUNCTION_INFO_V1(complex_send);
-
-Datum
-complex_send(PG_FUNCTION_ARGS)
-{
-    Complex    *complex = (Complex *) PG_GETARG_POINTER(0);
-    StringInfoData buf;
-
-    pq_begintypsend(&buf);
-    pq_sendfloat8(&buf, complex->x);
-    pq_sendfloat8(&buf, complex->y);
-    PG_RETURN_BYTEA_P(pq_endtypsend(&buf));
+    PG_RETURN_DATUM (d);//POINTER (result);//gHist);
 }
