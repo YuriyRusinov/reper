308c15d2d2e3a7213ba632caef738dbaf79f7042
yuriyrusinov <yuriyrusinov@05742d63-fc59-4ccd-b3f0-d7e2a2a8a88d>
2013-12-10 15:39:30 +0000

    1st row histogram.
    Signed-off-by: Yuriy Rusinov <yrusinov@gmail.com>
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/dynamicdocs/code@777 05742d63-fc59-4ccd-b3f0-d7e2a2a8a88d

--- ddocserver/internals/libfloader/src/hist.c
+++ ddocserver/internals/libfloader/src/hist.c
@@ -60,61 +60,73 @@ Datum histogram(PG_FUNCTION_ARGS)
     SPI_finish ();
     pfree (sql);
     elog (INFO, "Reading was completed\n");
-//    Oid * oids = (Oid *)palloc (2*sizeof (Oid));
-//    oids[0] = FLOAT8OID;
-//    oids[1] = FLOAT8OID;
-    if (get_call_result_type (fcinfo, NULL, &tupdesc) != TYPEFUNC_COMPOSITE)
-    {
-        elog (ERROR, "Incorrect transformation");
-        PG_RETURN_NULL ();
-    }
-    else
-        elog (INFO, "Result init, number of attributes = %d", tupdesc->natts);
+//    if (get_call_result_type (fcinfo, NULL, &tupdesc) != TYPEFUNC_COMPOSITE)
+//    {
+//        elog (ERROR, "Incorrect transformation");
+//        PG_RETURN_NULL ();
+//    }
+//    else
+//        elog (INFO, "Result init, number of attributes = %d", tupdesc->natts);
 
     FuncCallContext *funcctx;
     int call_cntr;
     int max_calls;
     AttInMetadata *attinmeta;
     unsigned long il;
+    TupleDesc tupdescRes;
     if (SRF_IS_FIRSTCALL())
     {
         MemoryContext   oldcontext;
         funcctx = SRF_FIRSTCALL_INIT();
         oldcontext = MemoryContextSwitchTo(funcctx->multi_call_memory_ctx);
         funcctx->max_calls = num;
-        if (get_call_result_type(fcinfo, NULL, &tupdesc) != TYPEFUNC_COMPOSITE)
+        Oid * oids = (Oid *)palloc (2*sizeof (Oid));
+        oids[0] = INT8OID;
+        oids[1] = FLOAT8OID;
+        if (get_call_result_type(fcinfo, oids, &tupdescRes) != TYPEFUNC_COMPOSITE)
             ereport(ERROR,
                     (errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
                      errmsg("function returning record called in context "
                             "that cannot accept type record")));
-        attinmeta = 0;//TupleDescGetAttInMetadata(tupdesc);
-        funcctx->attinmeta = 0;//attinmeta;
+        attinmeta = TupleDescGetAttInMetadata(tupdescRes);
+        funcctx->attinmeta = attinmeta;
         MemoryContextSwitchTo(oldcontext);
-        funcctx->tuple_desc = BlessTupleDesc( tupdesc );
+        //funcctx->tuple_desc = BlessTupleDesc( tupdescRes );
+        pfree (oids);
         elog (INFO, "1st row");
     }
     funcctx = SRF_PERCALL_SETUP();
     call_cntr = funcctx->call_cntr;
     max_calls = funcctx->max_calls;
+    elog (INFO, "number of rows = %d, number of columns are %d", max_calls, tupdescRes->natts );
     attinmeta = funcctx->attinmeta;
     HeapTuple tuple;
     if (call_cntr < max_calls)
     {
         elog (INFO, "Rows");
         il = call_cntr;
-        Datum * hvalues = (Datum *)palloc (2*sizeof(Datum));
+        //Datum * hvalues = (Datum *)palloc (2*sizeof(Datum));
+
+        char ** svals = (char **) palloc (2*sizeof (char*));
+        svals [0] = (char *) palloc(5 * sizeof(char));
+        svals [1] = (char *) palloc(20 * sizeof(char));
         double val = gsl_histogram_get (gHist, il);
-        hvalues[0] = UInt32GetDatum (il);
-        hvalues[1] = Float8GetDatum (val);
+        snprintf (svals[0], 5, "%lu", il);
+        snprintf (svals[1], 20, "%lf", val);
+        //hvalues[0] = UInt32GetDatum (il);
+        //hvalues[1] = Float8GetDatum (val);
         bool * nulls = palloc( 2 * sizeof( bool ) );
 
         elog (INFO, "%lu", il);
-        tuple = heap_form_tuple( tupdesc, hvalues, nulls);
+        tuple = BuildTupleFromCStrings (attinmeta, svals);//heap_form_tuple( tupdescRes, hvalues, nulls);
         elog (INFO, "%lu", il);
-        //BuildTupleFromCStrings (attinmeta, hvalues);
+        Datum result = HeapTupleGetDatum(tuple);
         pfree (nulls);
-        pfree (hvalues);
-        SRF_RETURN_NEXT(funcctx, HeapTupleGetDatum( tuple ) );
+        pfree (svals[1]);
+        pfree (svals[0]);
+        pfree (svals);
+        //pfree (hvalues);
+        SRF_RETURN_NEXT(funcctx, result);//HeapTupleGetDatum( tuple ) );
     }
     else
     {
