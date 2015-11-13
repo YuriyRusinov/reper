6bfe70adb30dd27897d2e71bebe92c19c16bf630
Yuriy Rusinov <yrusinov@gmail.com>
2015-11-13 19:25:34 +0300

    Test of array parameters.
    Signed-off-by: Yuriy Rusinov <yrusinov@gmail.com>

---
+++ io_server/internals/libfloader/src/point_test.c
@@ -0,0 +1,19 @@
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_randist.h>
+#include <stdio.h>
+#include <postgres.h>
+#include "floader.h"
+
+PG_FUNCTION_INFO_V1 (test_function_array);
+
+Datum test_function_array (PG_FUNCTION_ARGS)
+{
+    VarChar *pArr = PG_GETARG_CHAR (0);
+
+    int spi_res;
+    spi_res = SPI_connect();
+    SPI_finish();
+
+
+    PG_RETURN_FLOAT8 (0.0);
+}
