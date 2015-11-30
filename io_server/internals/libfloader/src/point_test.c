897e43a312b338255052098e5bb1c4949422e05d
Yuriy Rusinov <yrusinov@gmail.com>
2015-12-01 00:22:21 +0300

    SPI_execute_with_args w\prototype.
    Signed-off-by: Yuriy Rusinov <yrusinov@gmail.com>

--- io_server/internals/libfloader/src/point_test.c
+++ io_server/internals/libfloader/src/point_test.c
@@ -2,6 +2,8 @@
 #include <gsl/gsl_randist.h>
 #include <stdio.h>
 #include <postgres.h>
+#include <executor/spi.h>
+#include <utils/builtins.h>
 #include "floader.h"
 
 PG_FUNCTION_INFO_V1 (square_image);
@@ -9,6 +11,9 @@ PG_FUNCTION_INFO_V1 (square_image);
 Datum square_image (PG_FUNCTION_ARGS)
 {
     bytea * imageData = PG_GETARG_BYTEA_P(0);
+    elog (INFO, "argument is %s\n", imageData);
+//    text * tImageData = PG_GETARG_TEXT_P (0);
+//    char * bImageStr = text_to_cstring (tImageData);
 
     int spi_res;
     spi_res = SPI_connect();
@@ -17,19 +22,47 @@ Datum square_image (PG_FUNCTION_ARGS)
         elog(ERROR, "Cannot connect via SPI");
         PG_RETURN_NULL();
     }
-    int nr_length = strlen ("select length(null::bytea);");
-    size_t n = length (imageData);
+    int nr_length = strlen ("select length($1::bytea);");
+    size_t n = sizeof (imageData);
+    char * command_image_size = (char *)palloc(nr_length+1);
+    const int nargs = 1;
+    Oid * oids = (Oid *)palloc (nargs*sizeof (Oid));
+    oids[0] = BYTEAOID;
+    Datum * vals = (Datum *)palloc (nargs*sizeof (Datum));
+    snprintf (command_image_size, nr_length, "select length($1::bytea);");
+    vals[0] = PointerGetDatum (imageData);
+    const char * nulls = 0;
+    int rSize = SPI_execute_with_args (command_image_size, nargs, oids, vals, nulls, true, 1L);
+    int idproc = SPI_processed;
+    if (rSize != SPI_OK_SELECT || idproc != 1)
+    {
+        SPI_finish ();
+        pfree (oids);
+        pfree (vals);
+        PG_RETURN_FLOAT8 (rSize);
+    }
+    TupleDesc tupdesc = SPI_tuptable->tupdesc;
+    SPITupleTable *tuptable = SPI_tuptable;
+    HeapTuple tuple = tuptable->vals[0];
+    char * n_str = SPI_getvalue (tuple, tupdesc, 1);
+    n = atoi (n_str);
+    elog(INFO, "image data, size is  %zu\n", n);
+/*
     char * xstr = "\\x";
-    if (strstr ((char *)imageData, xstr))
+
+     if (position (xstr, imageData)==1)//strstr ((char *)imageData, xstr))
     {
         elog(INFO, "image data was hexadecimal, size is %zu\n", n);
-        PG_RETURN_FLOAT8 (1.0);
+        //PG_RETURN_FLOAT8 (1.0);
     }
     else
     {
         elog(INFO, "image data, size is  %zu\n", n);
-        PG_RETURN_FLOAT8 (-1.0);
+        //PG_RETURN_FLOAT8 (-1.0);
     }
+*/
+    pfree (oids);
+    pfree (vals);
     SPI_finish();
 
     PG_RETURN_FLOAT8 (0.0);
